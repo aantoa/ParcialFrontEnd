9168b8d2d0ccd9cb81001969cb967240ce2008fc
aanahia <anahi2654@gmail.com>
2024-08-12 20:49:36 -0500

    Primer commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import CardStyles from "../Styles/Card.module.css";
+
+const Card = () => {
+  return (
+    <div className={CardStyles.card}>Hola, soy una tarjeta</div>
+  )
+}
+
+export default Card
